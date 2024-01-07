058805c97c92a1171be565ce8ea5da2e98ea847a
viv0812 <154576071+viv0812@users.noreply.github.com>
2024-01-07 21:27:44 +0530

    React router

---
+++ login_page/log/public/router/route/src/component/about.js
@@ -0,0 +1,15 @@
+
+
+import React from "react";
+
+function About() {
+	return (
+		<div>
+			<h2>
+				Student @ SREC, pursuing Information Technology.
+			</h2>
+	
+		</div>
+	);
+}
+export default About;
---
+++ login_page/log/public/router/route/src/component/contact.js
@@ -0,0 +1,18 @@
+
+
+import React from "react";
+
+function Contact() {
+	return (
+		<address>
+			You can find us here:
+			<br />
+			viv
+			<br />
+			5th & 6th Floor, Royal Kapsons, A- 118, <br />
+			Sector- 136, Noida, Uttar Pradesh (201305)
+		</address>
+	);
+}
+
+export default Contact;
